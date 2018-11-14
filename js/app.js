d3be35c8aff8d7e0f66c6c79705579971591d926
Héctor Gómez Reyes <hector.agr@gmail.com>
2018-11-14 13:04:04 -0800

    Initial Draw
    New:
      - Added Player Class with constructor and update methods
      - Added handleInput for Player to move on keyup inside Canvas boundary
      - Viewport mobile so it doesn't shrink in smaller displays
    Changes:
      - Changed Enemy from ES5 to ES6 Class

--- js/app.js
+++ js/app.js
@@ -1,34 +1,71 @@
 // Enemies our player must avoid
-var Enemy = function() {
+class Enemy{
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-};
+    constructor(x = -10, y = 70, speed = Math.random()*5+2){
+        this.sprite = 'images/enemy-bug.png';
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+    }
+    
+    // Update the enemy's position, required method for game
+    // Parameter: dt, a time delta between ticks
+    update(dt){
+        // You should multiply any movement by the dt parameter
+        // which will ensure the game runs at the same speed for
+        // all computers.
+        if (this.x < 401) {
+            this.x += this.speed;
+        }else{
+            this.x = -10;
+            this.speed = Math.random()*5+2;
+        }
+    }
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-};
+    // Draw the enemy on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
+    }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+class Player{
+    constructor(x = 200, y = 300) {
+        this.sprite = 'images/char-boy.png';
+        this.x = x;
+        this.y = y;
+    }
 
+    update(dt) {
+        
+    }
 
+    handleInput(direction) {
+        if (direction == 'left' && this.x > 0) {
+            this.x -= 101;
+        }else if(direction == 'right' && this.x < 401) {
+            this.x += 101;
+        }else if(direction == 'up' && this.y > -20) {
+            this.y -= 80;
+        }else if (direction == 'down' && this.y < 380) {
+            this.y += 80;
+        }
+        console.log(`${this.x},${this.y}`);
+    }
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);    
+    }
+}
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+let allEnemies = [new Enemy(), new Enemy(0, 140), new Enemy(-10, 210)];
+let player = new Player();
 
 
 
